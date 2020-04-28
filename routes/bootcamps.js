ce87a648168ae11fe4a8d6cd3c952270ffeed155
Florent VICENTE <vicentegroupe@gmail.com>
2020-04-28 12:22:11 +0200

    geospatial query Get bootcamps within radius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,13 +1,21 @@
 const express = require("express")
 
 // Je récupère mes fonctions pour chaque route depuis /controllers/bootcamps
-const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp } = require("../controllers/bootcamps")
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius
+} = require("../controllers/bootcamps")
 
 const router = express.Router()
 
 // J'exporte mes routes vers server.js où elles seront "mount"
 // J'ai rajouté aux routes les fonctions récupérées depuis /controllers/bootcamps
 // Je différencie celles nécessitant un id des autres
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius)
 router.route("/").get(getBootcamps).post(createBootcamp)
 router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
