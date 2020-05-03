cb8e243d70e972aa66e810b0b764c9bd37a4ec08
Florent VICENTE <vicentegroupe@gmail.com>
2020-05-03 10:39:08 +0200

    update & delete course

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,12 @@
 const express = require("express")
 
 // Je récupère mes fonctions pour chaque route depuis /controllers/bootcamps
-const { getCourses, getCourse, addCourse } = require("../controllers/courses")
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require("../controllers/courses")
 
 const router = express.Router({ mergeParams: true })
 
 router.route("/").get(getCourses).post(addCourse)
-router.route("/:id").get(getCourse)
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse)
 
 
 module.exports = router
