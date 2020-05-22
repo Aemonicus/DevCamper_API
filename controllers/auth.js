5a703dc00fec466ced9969c1d6065a4aedaa157e
Florent VICENTE <vicentegroupe@gmail.com>
2020-05-22 17:22:04 +0200

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User")
 
 /**
  * Register User
- * route: GET /api/v1/auth/register
+ * route: POST /api/v1/auth/register
  * access: Public
  */
 
@@ -18,6 +18,40 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role
   })
+
+  // Create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token })
+})
+
+/**
+ * Login User
+ * route: POST /api/v1/auth/login
+ * access: Public
+ */
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide and email and password", 400))
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password")
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid email and/or password", 401))
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password)
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid email and/or password", 401))
+  }
 
   // Create token
   const token = user.getSignedJwtToken()
